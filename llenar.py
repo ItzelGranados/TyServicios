417a5d48e199ca63a3baa582a317514cb0d362a6
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2022-09-06 13:42:49 -0500

    Solución para la población de modelos (CriterioResolucion)

--- llenar.py
+++ llenar.py
@@ -1,30 +1,19 @@
+from posts.models import CriterioResolucion
 import csv
+import os
+import django
 
-import os, django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TyServicios.settings")  # project_name nombre del proyecto
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TyServicios.settings")
 django.setup()
 
-from posts.models import CriterioResolucion
-
 with open('Matriz/csv/CriterioResolucion.csv', 'r') as file:
+    CriterioResolucion.objects.all().delete()
     reader = csv.reader(file)
     headers = next(reader)
     i = 2
     for row in reader:
         resolucion = CriterioResolucion(nombre_criterio=row[2], nombre=row[5],
                                         articulo=row[6], fraccion=row[7])
-        try:
-            resolucion.save()
-            print("Save", i)
-        except:
-            #resolucion.save()
-            print("Listo", i)
-            i = i+1
-'''
-        try:
-            #resolucion.save()
-        except:
-            print("error")
-
-'''
+        resolucion.save()
+        print("Listo", i)
+        i = i + 1
